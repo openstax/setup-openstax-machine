43c7b0157389c6d54e2f5c5beb955e91a405077a
m1yag1 <mike.arbelaez@rice.edu>
2018-02-21 16:01:22 -0600

    attempt to run ansible

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,6 +5,21 @@ set -e
 cd "$(dirname "$0")"
 cd ..
 
+# Define a hash of roles
+roles=( ["CM"]= "content_manager")
+
+# Setup some variables
+if [ ! -n "$OXDIR"]; then
+  OXDIR=~/.ox-setup
+fi
+OXANSIBLE=$OXDIR/ansible
+
+echo -n "Enter your two letter role at OpenStax from one of these choices:
+\n CM - Content Manager \n"
+read role_input
+
+ROLE=roles[role_input]
+
 # Shared functions
 pretty_print() {
 	printf "\n%b\n" "$1"
@@ -19,3 +34,26 @@ if ! command -v brew &>/dev/null; then
 else
   pretty_print "You already have Homebrew installed...good job!"
 fi
+
+# Install Python3
+pretty_print "Installing Python 3.6"
+brew install python3
+
+# Install Python3
+pretty_print "Installing ansible"
+brew install ansible
+
+# Install git
+pretty_print "Installing git"
+brew install git
+
+# Clone the setup-openstax-machine repo
+pretty_print "Cloning the installation playbook"
+env git clone --depth=1 https://github.com/m1yag1/setup-openstax-machine $OXDIR || {
+    pretty_print "Error: git clone of setup-openstax-machine repo failed\n"
+    exit 1
+  }
+
+# Run the playbook
+pretty_print "Running the ansible playbook. You will need to enter your password."
+    ansible-playbook -i $OXANSIBLE/inventory $OXANSIBLE/local_install.yml --ask-become-pass
